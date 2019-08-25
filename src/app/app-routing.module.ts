d9af13829e5200821378b3b56323804cee707bf7
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-08-25 20:19:02 +0300

    import relevant components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import {  RepositoriesComponent} from './repositories/repositories.component';
+import { SearchComponent } from './search/search.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'mygithub', component: MainComponent },
-  { path: 'repositories/:id', component: MainComponent },
+  { path: 'repositories/:id', component: RepositoriesComponent },
   { path: '', redirectTo:"/mygithub", pathMatch:"full"},
   { path:'**', component:NotFoundComponent},
 ];
