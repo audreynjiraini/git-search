e9d343c295f58ac4dc516d8d5ec876e44746cd9f
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-08-25 13:15:05 +0300

    import git service

--- src/app/services/git-service.service.ts
+++ src/app/services/git-service.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitServiceService {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
+
+  getUserDetails() {
+    return this.http.get("https://api.github.com/users/audreynjiraini?access_token=43085be5dd5dc02227ff26beccddea3625b76fe2")
+  }
 }
