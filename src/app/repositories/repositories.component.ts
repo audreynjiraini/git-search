5076ffa7a94ac2e0944bb5b1a629a441e8a00e62
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-08-26 09:48:59 +0300

    fix error in repositories path

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GitServiceService } from '../services/git-service.service';
+import { Users } from '../users';
+import { Repos } from '../repos';
 
 @Component({
   selector: 'app-repositories',
@@ -7,35 +9,35 @@ import { GitServiceService } from '../services/git-service.service';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
-  user1;
-  repos;
+  user1: Users;
+  repos: Repos;
   userName: string;
 
   constructor(public gitService: GitServiceService) {
-
-    // this.gitService.getUserDetails().subscribe(users => {
-    //   this.user1 = users;
-    //   console.log(this.user1);
-    // });
-    // this.gitService.getRepos().subscribe(result => {
-    //   this.repos = result;
-    //   console.log(this.repos);
-    // });
   }
 
-    findUser(){
-    //   this.gitService.updateUserName(this.userName);
-    //   this.gitService.getUserDetails().subscribe(users => {
-    //   this.user1 = users;
-    //   console.log(this.user1);
-    // });
-    // this.gitService.getRepos().subscribe(result => {
-    //   this.repos = result;
-    //   console.log(this.repos);
-    // });
+  findUser(userName) {
+    this.gitService.getUserDetails(userName).then(
+      () => {
+        this.user1 = this.gitService.userNames;
+        console.log(this.gitService.userNames);
+      },
+      (error) => {
+        console.log(error);
+      });
+    this.gitService.allRepos(userName).then(
+      (results) => {
+        this.repos = this.gitService.Repositories
+        console.log(this.repos);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   ngOnInit() {
+    this.findUser("audreynjiraini")
   }
 
 }
